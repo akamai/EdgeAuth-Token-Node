f0b9846e53baa28573812e6bdf6574041d6dfcad
goodjoon <goodjoon@gmail.com>
2017-04-17 16:48:04 +0900

    v0.1.0 without test codes

--- examples/generateToken.js
+++ examples/generateToken.js
@@ -5,20 +5,35 @@
 var AuthToken = require('../lib/akamai/authtoken/authtoken');
 
 var atk = new AuthToken({
-    key: "7a5c2be9f42a1f32cd889d1cf775f05d0cd162bd2871d00fe705ab28c9bba5a5"
-    ,startTime: new Date()
-    ,windowSeconds: 2*60*60
-    ,accessList: ["/test/*","/sample/*"]
-    ,ipAddress: '192.168.0.1 ipv6 입니다 : af:1a:3d:99:04:0f'
+    tokenName: "token"
+    ,key: "7a5c2be9f42a1f32cd889d1cf775f05d0cd162bd2871d00fe705ab28c9bba5a5"
+    ,startTime: 1492404013
+    ,windowSeconds: 604800
+    ,accessList: ["/*"]
 });
-atk.generateToken();
 
-atk.setOptions({key:'abc', accessList:['/*']});
-atk.generateToken();
+console.log("GENERATED TOKEN: " + atk.generateToken());
 
+console.log("=== Modifying existing options ===");
+atk.setOptions(
+    {
+        escapeEarly: true
+        ,escapeEarlyUpper: true
+        ,algorithm: 'md5'
+    }
+    ,true
+);
+console.log("GENERATED TOKEN: " + atk.generateToken());
 
-
-console.log("=== END ===");
-
+console.log("=== Replacing existing options ===");
+atk.setOptions(
+    {
+        algorithm: 'sha1'
+        ,key: "7a5c2be9f42a1f32cd889d1cf775f05d0cd162bd2871d00fe705ab28c9bba5a5"
+        ,startTime: new Date()
+        ,accessList: ['/example1','/example2']
+    }
+);
+console.log("GENERATED TOKEN: " + atk.generateToken());
 
 //AuthToken.generateUrl();
